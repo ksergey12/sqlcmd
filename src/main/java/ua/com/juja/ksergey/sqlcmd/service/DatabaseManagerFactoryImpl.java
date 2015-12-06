2dc859741cd157466ca7d1f047dac8105861234f
user <netangel@inbox.ru>
2015-12-06 23:12:12 +0200

    Added DatabaseManagerFactory interface.

---
+++ src/main/java/ua/com/juja/ksergey/sqlcmd/service/DatabaseManagerFactoryImpl.java
@@ -0,0 +1,17 @@
+package ua.com.juja.ksergey.sqlcmd.service;
+
+import org.springframework.stereotype.Component;
+import ua.com.juja.ksergey.sqlcmd.model.DatabaseManager;
+import ua.com.juja.ksergey.sqlcmd.model.JDBCDatabaseManager;
+
+/**
+ * Created by user on 06.12.15.
+ */
+@Component
+public class DatabaseManagerFactoryImpl implements DatabaseManagerFactory {
+
+    @Override
+    public DatabaseManager createDatabaseManager(){
+        return new JDBCDatabaseManager();
+    }
+}
