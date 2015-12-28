ab595c9b89da79ed114467029a87f5be7e4ebd3f
User <netangel@inbox.ru>
2015-12-28 23:57:20 +0200

    Added Add and Edit commands.

--- src/main/java/ua/com/juja/ksergey/sqlcmd/controller/MainController.java
+++ src/main/java/ua/com/juja/ksergey/sqlcmd/controller/MainController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import ua.com.juja.ksergey.sqlcmd.model.DataSet;
+import ua.com.juja.ksergey.sqlcmd.model.DataSetImpl;
 import ua.com.juja.ksergey.sqlcmd.model.DatabaseManager;
 import ua.com.juja.ksergey.sqlcmd.service.Service;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 /**
  * Created by User on 25.12.2015.
@@ -21,7 +23,13 @@ public class MainController {
     private Service service;
 
     @RequestMapping(value = {"/menu", "/"}, method = RequestMethod.GET)
-    public String menu(HttpServletRequest request) {
+    public String menu(HttpServletRequest request, HttpSession session) {
+        DatabaseManager manager = getManager(session);
+
+        if (manager == null) {
+            return "redirect:/connect";
+        }
+
         request.setAttribute("items", service.commandsList());
         return "menu";
     }
@@ -65,6 +73,63 @@ public class MainController {
         return "show";
     }
 
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String add(HttpServletRequest request, HttpSession session) {
+        DatabaseManager manager = getManager(session);
+
+        if (manager == null) {
+            return "redirect:/connect";
+        }
+
+        String table = request.getParameter("table");
+        request.setAttribute("tableName", table);
+        request.setAttribute("tableHeader", service.showHeader(manager, table));
+        return "add";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String adding(HttpServletRequest request, HttpSession session) {
+        String table = request.getParameter("table");
+
+        try {
+            DatabaseManager manager = (DatabaseManager) session.getAttribute("db_manager");
+            List<String> tableHeader = service.showHeader(manager, table);
+            DataSet input = getDataSet(request, tableHeader);
+            service.create(manager, table, input);
+            return "redirect:/show?table="  + table;
+        } catch (Exception e) {
+            request.setAttribute("message", e.getMessage());
+            return "error";
+        }
+    }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.GET)
+    public String edit(HttpServletRequest request, HttpSession session) {
+        DatabaseManager manager = (DatabaseManager) session.getAttribute("db_manager");
+        String table = request.getParameter("table");
+        request.setAttribute("tableName", table);
+        request.setAttribute("tableHeader", service.showHeader(manager, table));
+        request.setAttribute("id", request.getParameter("id"));
+        return "edit";
+    }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.POST)
+    public String editing(HttpServletRequest request, HttpSession session) {
+        String table = request.getParameter("table");
+        int id = Integer.parseInt(request.getParameter("id"));
+
+        try {
+            DatabaseManager manager = (DatabaseManager) session.getAttribute("db_manager");
+            List<String> tableHeader = service.showHeader(manager, table);
+            DataSet input = getDataSet(request, tableHeader);
+            service.update(manager, table, input, id);
+            return "redirect:/show?table="  + table;
+        } catch (Exception e) {
+            request.setAttribute("message", e.getMessage());
+            return "error";
+        }
+    }
+
     @RequestMapping(value = "/connect", method = RequestMethod.GET)
     public String connect(HttpSession session) {
 
@@ -85,7 +150,7 @@ public class MainController {
             DatabaseManager manager = service.connect(database, user, password);
             session.setAttribute("db_manager", manager);
             session.setAttribute("database", database);
-            return "redirect:/menu";
+            return "redirect:/list";
         } catch (Exception e) {
             request.setAttribute("message", e.getMessage());
             return "error";
@@ -94,5 +159,13 @@ public class MainController {
 
     private DatabaseManager getManager(HttpSession session) {
         return (DatabaseManager) session.getAttribute("db_manager");
+    }
+
+    private DataSet getDataSet(HttpServletRequest req, List<String> tableHeader) {
+        DataSet input = new DataSetImpl();
+        for(String element : tableHeader){
+            input.put(element, req.getParameter(element));
+        }
+        return input;
     }
 }
