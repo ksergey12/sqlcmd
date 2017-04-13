a4e841cfe76dc12b6146620f00cb1b06954ee929
User <netangel@inbox.ru>
2017-04-13 18:23:13 +0300

    MainServlet refactoring.

--- src/main/java/net/sqlcmd/controller/MainServlet.java
+++ src/main/java/net/sqlcmd/controller/MainServlet.java
@@ -33,32 +33,22 @@ public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = getAction(request);
-        DatabaseManager manager = getManager(request);
+        DatabaseManager manager = getManager(request, response);
 
         if (action.startsWith("/connect")) {
             if (manager == null) {
-//                request.getRequestDispatcher("connect.jsp").forward(request, response);
                 jsp("connect", request, response);
             } else {
-//                response.sendRedirect(response.encodeRedirectURL("list"));
                 redirect("list", response);
             }
             return;
         }
 
-        if (manager == null) {
-//            response.sendRedirect(response.encodeRedirectURL("connect"));
-            redirect("connect", response);
-            return;
-        }
-
         if (action.startsWith("/menu") || action.equals("/")) {
             request.setAttribute("items", service.commandsList());
-//            request.getRequestDispatcher("menu.jsp").forward(request, response);
             jsp("menu", request, response);
 
         } else if (action.startsWith("/help")) {
-//            request.getRequestDispatcher("help.jsp").forward(request, response);
             jsp("help", request, response);
 
         } else if (action.startsWith("/show")) {
@@ -66,14 +56,12 @@ public class MainServlet extends HttpServlet {
             request.setAttribute("tableName", table);
             request.setAttribute("tableHeader", service.showHeader(manager, table));
             request.setAttribute("table", service.showTable(manager, table));
-//            request.getRequestDispatcher("show.jsp").forward(request, response);
             jsp("show", request, response);
 
         } else if (action.startsWith("/add")) {
             String table = request.getParameter("table");
             request.setAttribute("tableName", table);
             request.setAttribute("tableHeader", service.showHeader(manager, table));
-//            request.getRequestDispatcher("add.jsp").forward(request, response);
             jsp("add", request, response);
 
         } else if (action.startsWith("/edit")) {
@@ -81,7 +69,6 @@ public class MainServlet extends HttpServlet {
             request.setAttribute("tableName", table);
             request.setAttribute("tableHeader", service.showHeader(manager, table));
             request.setAttribute("id", request.getParameter("id"));
-//            request.getRequestDispatcher("edit.jsp").forward(request, response);
             jsp("edit", request, response);
 
         } else if (action.startsWith("/list")) {
@@ -94,94 +81,87 @@ public class MainServlet extends HttpServlet {
             String table = request.getParameter("table");
             request.setAttribute("table", table);
             service.clear(manager, table);
-//            request.getRequestDispatcher("clear.jsp").forward(request, response);
             jsp("clear", request, response);
 
         }else if (action.startsWith("/dropTable")) {
             String table = request.getParameter("table");
             request.setAttribute("table", table);
             service.dropTable(manager, table);
-//            request.getRequestDispatcher("drop.jsp").forward(request, response);
             jsp("drop", request, response);
 
         }else if (action.startsWith("/createTable")) {
-//            request.getRequestDispatcher("createTable.jsp").forward(request, response);
             jsp("createTable", request, response);
 
         } else if (action.startsWith("/exit")) {
             request.getSession(false).invalidate();
-//            request.getRequestDispatcher("connect.jsp").forward(request, response);
             jsp("connect", request, response);
 
         } else {
             request.setAttribute("message", "Страница не найдена!");
-//            request.getRequestDispatcher("error.jsp").forward(request, response);
             jsp("error", request, response);
         }
     }
 
-    private String getAction(HttpServletRequest req) {
-        String requestURI = req.getRequestURI();
-        return requestURI.substring(req.getContextPath().length(), requestURI.length());
-    }
-
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String action = getAction(req);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = getAction(request);
 
         if (action.startsWith("/connect")) {
-            String database = req.getParameter("dbname");
-            String user = req.getParameter("username");
-            String password = req.getParameter("password");
+            String database = request.getParameter("dbname");
+            String user = request.getParameter("username");
+            String password = request.getParameter("password");
 
             try {
                 DatabaseManager manager = service.connect(database, user, password);
-                req.getSession().setAttribute("db_manager", manager);
-                req.getSession().setAttribute("database", database);
-                resp.sendRedirect(resp.encodeRedirectURL("list"));
+                request.getSession().setAttribute("db_manager", manager);
+                request.getSession().setAttribute("database", database);
+                response.sendRedirect(response.encodeRedirectURL("list"));
             } catch (Exception e) {
-                req.setAttribute("message", e.getMessage());
-                req.getRequestDispatcher("error.jsp").forward(req, resp);
+                request.setAttribute("message", e.getMessage());
+                jsp("error", request, response);
             }
 
         } else if (action.startsWith("/add")) {
-            String table = req.getParameter("table");
+            String table = request.getParameter("table");
 
             try {
-                DatabaseManager manager = (DatabaseManager) req.getSession().getAttribute("db_manager");
+                DatabaseManager manager = (DatabaseManager) request.getSession().getAttribute("db_manager");
                 List<String> tableHeader = service.showHeader(manager, table);
-                DataSet input = getDataSet(req, tableHeader);
+                DataSet input = getDataSet(request, tableHeader);
                 service.create(manager, table, input);
-                resp.sendRedirect(resp.encodeRedirectURL("show?table=" + table));
+                redirect("show?table=" + table, response);
+
             } catch (Exception e) {
-                req.setAttribute("message", e.getMessage());
-                req.getRequestDispatcher("error.jsp").forward(req, resp);
+                request.setAttribute("message", e.getMessage());
+                jsp("error", request, response);
             }
 
         } else if (action.startsWith("/edit")) {
-            String table = req.getParameter("table");
-            int id = Integer.parseInt(req.getParameter("id"));
+            String table = request.getParameter("table");
+            int id = Integer.parseInt(request.getParameter("id"));
 
             try {
-                DatabaseManager manager = (DatabaseManager) req.getSession().getAttribute("db_manager");
+                DatabaseManager manager = (DatabaseManager) request.getSession().getAttribute("db_manager");
                 List<String> tableHeader = service.showHeader(manager, table);
-                DataSet input = getDataSet(req, tableHeader);
+                DataSet input = getDataSet(request, tableHeader);
                 service.update(manager, table, input, id);
-                resp.sendRedirect(resp.encodeRedirectURL("show?table=" + table));
+                redirect("show?table=" + table, response);
+
             } catch (Exception e) {
-                req.setAttribute("message", e.getMessage());
-                req.getRequestDispatcher("error.jsp").forward(req, resp);
+                request.setAttribute("message", e.getMessage());
+                jsp("error", request, response);
             }
         } else if (action.startsWith("/createTable")) {
-            String table = req.getParameter("table");
+            String table = request.getParameter("table");
 
             try {
-                DatabaseManager manager = (DatabaseManager) req.getSession().getAttribute("db_manager");
+                DatabaseManager manager = (DatabaseManager) request.getSession().getAttribute("db_manager");
                 service.createTable(manager, table);
-                resp.sendRedirect(resp.encodeRedirectURL("show?table=" + table));
+                redirect("show?table=" + table, response);
+
             } catch (Exception e) {
-                req.setAttribute("message", e.getMessage());
-                req.getRequestDispatcher("error.jsp").forward(req, resp);
+                request.setAttribute("message", e.getMessage());
+                jsp("error", request, response);
             }
         }
     }
@@ -194,12 +174,24 @@ public class MainServlet extends HttpServlet {
         return input;
     }
 
+    private String getAction(HttpServletRequest req) {
+        String requestURI = req.getRequestURI();
+        return requestURI.substring(req.getContextPath().length(), requestURI.length());
+    }
+
     private void redirect(String url, HttpServletResponse response) throws IOException {
         response.sendRedirect(response.encodeRedirectURL(url));
     }
 
-    private DatabaseManager getManager(HttpServletRequest request) {
-        return (DatabaseManager) request.getSession().getAttribute("manager");
+    private DatabaseManager getManager(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        DatabaseManager manager = (DatabaseManager) request.getSession().getAttribute("manager");
+
+        if (manager != null) {
+            return manager;
+        } else {
+            redirect("connect", response);
+            return DatabaseManager.NULL;
+        }
     }
 
     private void jsp(String jsp, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
